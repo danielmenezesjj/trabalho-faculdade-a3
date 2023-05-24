e697e4e1b039c1500f84d4d1cfa72b62b7d3f69a
Yasmin Santana <94482549+santyasm@users.noreply.github.com>
2023-05-24 20:24:24 -0300

    Validação usuarios

--- trabalho-faculdade-a3/src/DAO/UsuarioDAO.java
+++ trabalho-faculdade-a3/src/DAO/UsuarioDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
 import DTO.UsuarioDTO;
@@ -11,10 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author yasmi
- */
 public class UsuarioDAO {
     Connection conn;
     
@@ -27,8 +19,6 @@ public class UsuarioDAO {
             PreparedStatement pstm = conn.prepareStatement(sql);
             pstm.setString(1, objUsuarioDto.getCpf_usuario());
             pstm.setString(2, objUsuarioDto.getSenha_usuario());
-            // pstm.setString(3, String.valueOf(objUsuarioDto.getPerfil_usuario()));
-
             ResultSet rs = pstm.executeQuery();
             
             return rs;
