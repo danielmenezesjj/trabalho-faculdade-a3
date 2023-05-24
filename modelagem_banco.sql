765931c00518918c06f596f3248a6741dcd735da
Daniel <88933522+danielmenezesjj@users.noreply.github.com>
2023-05-24 20:10:16 -0300

    Update modelagem_banco.sql

--- modelagem_banco.sql
+++ modelagem_banco.sql
@@ -1,11 +1,8 @@
-CREATE DATABASE detran;
-
 use detran;
 
-
 CREATE TABLE perfil(
-					id int auto_increment primary key, 
-					nome_perfil varchar(50) NOT NULL
+	id int auto_increment primary key, 
+	nome_perfil varchar(50) NOT NULL
 );
 CREATE TABLE usuarios(
 	id int auto_increment primary key,
@@ -19,9 +16,34 @@ CREATE TABLE usuarios(
     perfil_id int not null,
     FOREIGN KEY (perfil_id) REFERENCES perfil(id)
 );
-insert into perfil(nome_perfil) value ("ADMIN");
-INSERT INTO usuarios(nome_completo, dt_nascimento, adm, cpf, email, telefone, senha, perfil_id) value("daniel menezes", '2000-12-25', true, "1234567891", "danielmenezes2512@outlook.com", "12356791", "123", 5);
 
+
+CREATE TABLE servicos(
+	id int auto_increment primary key,
+	nome_item varchar(100) not null,
+	preco_item FLOAT NOT NULL
+
+);
+
+
+create table carteira(
+id int auto_increment primary key,
+dt_emissao date NOT NULL,
+dt_vencimento date NOT NULL,
+aluno_id int NOT NULL,
+foreign key (aluno_id) references usuarios(id)
+);
+
+INSERT INTO usuarios(nome_completo, dt_nascimento, adm, cpf, email, telefone, senha, perfil_id) value("daniel menezes", '2000-12-25', true, "123456", "danielmenezes2512@outlook.com", "12356791", "123", 1);
+insert into perfil(nome_perfil) value ("ADMIN");
+insert servicos(nome_item, preco_item) value("Renovação ", 230.45);
+
+
+
+select * from servicos;
+select * from usuarios;
+select * from perfil;
+select * from carteira;
 
 
 
