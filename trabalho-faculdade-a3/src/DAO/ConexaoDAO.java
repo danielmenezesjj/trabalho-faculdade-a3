8cf4c4db30b512568dea70f03b96fe3a51c4a783
santyasm <yyasant5@gmail.com>
2023-05-29 13:31:51 -0300

    Correção de conflito na view ProvaTeorica

--- trabalho-faculdade-a3/src/DAO/ConexaoDAO.java
+++ trabalho-faculdade-a3/src/DAO/ConexaoDAO.java
@@ -14,7 +14,7 @@ public class ConexaoDAO{
         Connection conn = null;
         
         try{
-            String url = "jdbc:mysql://localhost:3306/detran?user=root&password=123456";
+            String url = "jdbc:mysql://localhost:3306/detran?user=root&password=root";
             conn = DriverManager.getConnection(url);
 
             System.out.println("DB connected!");
